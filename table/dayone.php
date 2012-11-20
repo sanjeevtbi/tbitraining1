374000beefe66721d1e30584967009db10907d25
student <student@student-HP-Pro-3330-MT>
2012-11-20 16:01:13 +0530

    change by sanjeev

--- table/dayone.php
+++ table/dayone.php
@@ -1,19 +1,23 @@
-<?php
-echo '<html><head><link rel="stylesheet" href="design.css" type="text/css"/></head><body>';
-echo "This is first file";
-$limit=$_POST['limit'];
-echo '<table id="tab">';
-for($i=1;$i<=$limit;$i++)	/* For loop for number of rows */
-{
-	echo '<tr height="10">';
-	for($j=1;$j<=$limit;$j++)	/* For loop for number of columns */
-	{
-		$a=$i*$j;
-		echo '<td>'.$a.'</td>';
+<html>
+<head>
+<link rel="stylesheet" href="design.css" type="text/css" />
+</head>
+<body>
+<table id="tab">
+<?php 	 
+for($i=1;$i<=$limit;$i++) /* For loop for number of rows */ { 
+?>		
+<tr height="10">
+<?php 
 
-	}	
-	echo '</tr>';				/*End loop*/
-}
-echo '</table></body></html>';
-php?>
+			for($j=1;$j<=$limit;$j++) /* For loop for number of columns */ {
+			$a=$i*$j; ?>
+			<td><?php echo '$a'; }  }?></td> 
+			 
+		</tr>
+		
+	</table>
+</body>
+</html>
+
 
